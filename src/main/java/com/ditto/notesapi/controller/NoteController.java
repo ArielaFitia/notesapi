3bace1790417a93d7f554fedc6031c6e6d867e6e
Ariela <arielafitiajoharini@gmail.com>
2024-12-24 19:57:00 +0300

    Tested API endpoints for notes

--- src/main/java/com/ditto/notesapi/controller/NoteController.java
+++ src/main/java/com/ditto/notesapi/controller/NoteController.java
@@ -22,23 +22,42 @@ public class NoteController {
         this.noteService = noteService;
     }
 
+    /**
+     * GET /notes - Retrieve all notes.
+     * @return List of all notes.
+     */
     @GetMapping
     public ResponseEntity<List<Note>> getAllNotes() {
         List<Note> note = noteService.getAllNotes();
         return ResponseEntity.ok(note);
     }
 
+    /**
+     * GET /notes/{id} - Retrieve note by ID.
+     * @param id ID of the note to retrieve.
+     * @return The requested note, or 404 Not Found if not found.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
         return noteService.getNoteById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
+    /**
+     * POST /notes - Create a new note.
+     * @param note The note to create.
+     * @return The created note.
+     */
     @PostMapping
     public ResponseEntity<Note> createNote(@RequestBody Note note) {
         Note createdNote = noteService.createNote(note);
         return ResponseEntity.ok(createdNote);
     }
 
+    /**
+     * DELETE /notes/{id} - Delete a note by ID.
+     * @param id ID of the note to delete.
+     * @return 204 No Content if successful, or 404 Not Found id the note doesn't exist.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
         if(noteService.getNoteById(id).isPresent()) {
